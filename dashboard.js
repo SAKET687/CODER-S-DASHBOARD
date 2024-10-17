1f56485802f2a8ec20a7198fe5594451d817adf7
Alit Indrawan <alitindrawan71@gmail.com>
2024-10-17 23:25:08 +0800

    feat: set session token

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,26 @@
+window.onload = function() {
+    // Function to check if a cookie exists
+    function getCookie(name) {
+        let cookieArr = document.cookie.split(";");
 
+        for (let i = 0; i < cookieArr.length; i++) {
+            let cookiePair = cookieArr[i].split("=");
+
+            // Removing whitespace at the beginning of the cookie name and compare it with the given string
+            if (name === cookiePair[0].trim()) {
+                return decodeURIComponent(cookiePair[1]);
+            }
+        }
+
+        // Return null if the cookie is not found
+        return null;
+    }
+
+    // Specify the cookie you are looking for (e.g., "sessionToken")
+    let sessionCookie = getCookie("sessionToken");
+
+    // Redirect to index page if cookie doesn't exist
+    if (!sessionCookie) {
+        window.location.href = "index.html";
+    }
+};
--- login.js
+++ login.js
@@ -21,10 +21,12 @@ const username1=localStorage.getItem("username");
 const password1=localStorage.getItem("password");
 if(username.length && password.length != 0){
     if( username == username1 && password == password1){
-alert ("Login successfully! You are entering in CODER'S DASHBOARD");
-window.location = "dashboard.html";
-return false;
-}
+        token = generateSessionToken()
+        setCookie("sessionToken", token, 7);
+        alert ("Login successfully! You are entering in CODER'S DASHBOARD");
+        window.location = "dashboard.html";
+        return false;
+    }
     else{
 alert ("Invalid User Id/Password");
 attempt -- ;
@@ -41,4 +43,25 @@ return false;
 else{
     alert("User Id/Password can't be empty! Please enter your User Id/Password.");
 }
+}
+
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (encodeURIComponent(value) || "") + expires + "; path=/";
+}
+
+function generateSessionToken() {
+    const randomValues = Array.from(crypto.getRandomValues(new Uint8Array(16)))
+        .map(value => ('0' + value.toString(16)).slice(-2))
+        .join('');
+    
+    const timestamp = Date.now().toString(36); // Convert timestamp to base-36
+    const sessionToken = `${randomValues}-${timestamp}`;
+    
+    return sessionToken;
 }
